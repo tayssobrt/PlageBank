950be316123be1e3c20ddfc3ad641798732317d8
tayssobrt <gui.andrevieira@gmail.com>
2026-01-29 20:03:57 -0300

    Update: criado metodo para validar valores negativos e nulos

--- src/models/Conta.java
+++ src/models/Conta.java
@@ -18,6 +18,20 @@ public class Conta {
     }
 
     private boolean validarValor(Double valor) {
+        //valida os valores de saque
+        if (valor == 0) {
+            System.out.println("Você não pode sacar um valor nulo");
+            return false;
+        } else if (valor < 0) {
+            System.out.println("Você não pode sacar um valor negativo");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validarSaldo(Double valor) {
+        //valida se o user tem saldo suficiente para o saldo
         if (valor > saldo) {
             System.out.println("Você não tem saldo suficiente!");
             return false;
@@ -25,6 +39,7 @@ public class Conta {
             return true;
         }
     }
+
 
 
 }
